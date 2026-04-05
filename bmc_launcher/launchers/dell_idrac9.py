83d2aac401e18ce4d6b330a99bcc2ea0c3b2ccad
Pritpal Sabharwal <pritpal@theopsguy.co.uk>
2026-04-05 15:41:02 -0400

    add launchers for dell idrac8 and idrac9

---
+++ bmc_launcher/launchers/dell_idrac9.py
@@ -0,0 +1,36 @@
+import logging
+import sys
+
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from bmc_launcher.launchers.abstract import BaseLauncher
+from bmc_launcher.model.configuration import Server
+
+log = logging.getLogger(__name__)
+
+
+class DellIdrac9Launcher(BaseLauncher):
+    def __init__(self, host: Server, driver):
+        super().__init__(host, driver)
+
+    def launch(self):
+        super().launch()
+        self.webdriver.get(self.url)
+
+        try:
+            WebDriverWait(self.webdriver, 30).until(EC.visibility_of_element_located((By.NAME, "username")))
+            username = self.webdriver.find_element("name", "username")
+            password = self.webdriver.find_element("name", "password")
+
+            username.send_keys(self.username)
+            password.send_keys(self.password)
+
+            WebDriverWait(self.webdriver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, "cux-button")))
+            self.webdriver.find_element(By.CLASS_NAME, "cux-button").click()
+            log.info("Login submitted.")
+        except (TimeoutException, NoSuchElementException) as e:
+            log.error("Login page interaction failed: %s", e)
+            sys.exit(1)
